7f4efc40528b038ac061e31a4896f07fe6606061
Alain <alain@MacBook-Pro.local>
2015-09-13 13:42:05 -0400

    Initial Commit

---
+++ Les amis de la science/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Les amis de la science
+//
+//  Created by Alain on 2015-09-13.
+//  Copyright (c) 2015 Production sur support. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
