ce1f3565bfafd3c20007baed7e14ff6c8e042f88
Alain <alain@MacBook-Pro.local>
2015-09-13 13:53:38 -0400

    Commit de départ

---
+++ module 03.solution/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  module 03.solution
+//
+//  Created by Alain on 2015-09-13.
+//  Copyright (c) 2015 Production sur support. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
